77c5be28d8faeb3fd31d9deddda053fa62dd3a38
marianadg <2269068@gmail.com>
2025-03-11 06:54:47 -0500

    cambio de nombre Entrenador y clase

--- Gym.cs
+++ Gym.cs
@@ -1,82 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+//transformé program en Gym
 
-// Interfaz para inscripción
-public interface IInscribible
-{
-    void Inscribirse();
-}
-
-// Clase base Persona
-public class Persona
-{
-    public string Nombre { get; private set; }
-    public int Edad { get; private set; }
-}
-
-// Clase Membresia
-public class Membresia
-{
-    public string Tipo { get; private set; }
-    public double Costo { get; private set; }
-    public DateTime FechaExpiracion { get; private set; }
-
-    public void MostrarDetalles()
-    {
-        Console.WriteLine($"Membresía: {Tipo}, Costo: {Costo}, Expira: {FechaExpiracion.ToShortDateString()}");
+class Gym {
+    static void Main() {
+        Gimnasio gimnasio = new Gimnasio();
+        Membresia membresia1 = new Membresia("Premium", 50.0, DateTime.Now.AddMonths(1));
+        Cliente cliente1 = new Cliente("Juan Alberto", 25, 70.5, 1.75, membresia1);
+        Entrenador entrenador1 = new Entrenador("Carlos", 30, "Musculación", "8:00 - 12:00");
+        Clase clase1 = new Clase("CrossFit", entrenador1, "10:00 - 11:00");
+        
+        gimnasio.AgregarMembresia(membresia1);
+        gimnasio.AgregarCliente(cliente1);
+        gimnasio.AgregarEntrenador(entrenador1);
+        gimnasio.AgregarClase(clase1);
+        
+        cliente1.Inscribirse();
+        entrenador1.CrearRutina();
+        clase1.MostrarInformacion();
+        membresia1.MostrarDetalles();
     }
 }
-
-// Cliente hereda de Persona
-public class Cliente : Persona, IInscribible
-{
-    public double Peso { get; private set; }
-    public double Altura { get; private set; }
-    public Membresia Membresia { get; private set; }
-
-    public void Inscribirse()
-    {
-        Console.WriteLine($"Cliente {Nombre} inscrito al gimnasio con membresía {Membresia.Tipo}.");
-    }
-}
-
-// Entrenador hereda de Persona
-public class Entrenador : Persona
-{
-    public string Especialidad { get; private set; }
-    public string Horario { get; private set; }
-
-    public void CrearRutina()
-    {
-        Console.WriteLine($"Entrenador {Nombre} ha creado una nueva rutina.");
-    }
-}
-
-// Clase Clase (entrenamiento impartido por los entrenadores)
-public class Clase
-{
-    public string Nombre { get; private set; }
-    public Entrenador Instructor { get; private set; }
-    public string Horario { get; private set; }
-
-    public void MostrarInformacion()
-    {
-        Console.WriteLine($"Clase: {Nombre}, Instructor: {Instructor.Nombre}, Horario: {Horario}");
-    }
-}
-
-// Clase Gimnasio
-public class Gimnasio
-{
-    private List<Cliente> clientes = new List<Cliente>();
-    private List<Entrenador> entrenadores = new List<Entrenador>();
-    private List<Clase> clases = new List<Clase>();
-    private List<Membresia> membresias = new List<Membresia>();
-}
-
-// Programa principal
-class Program
-{
-    static void Main() { }
-}
-
